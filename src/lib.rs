d5ba86c58ece9c123ca219ab2ec24a9daa3edeae
Henning Ottesen <henning@live.no>
2017-02-16 00:00:49 +0100

    Add error type and restrict to one instance
    Resolves: #8
    Resolves: #9

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,21 @@ use widestring::WideCString;
 use self::winapi::{c_int};
 use sys::*;
 
+use std::sync::atomic::{AtomicBool, Ordering, ATOMIC_BOOL_INIT};
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+
 pub const MONO_WIDTH: usize   = sys::MONO_WIDTH;
 pub const MONO_HEIGHT: usize  = sys::MONO_HEIGHT;
 pub const COLOR_WIDTH: usize  = sys::COLOR_WIDTH;
 pub const COLOR_HEIGHT: usize = sys::COLOR_HEIGHT;
 
+static INITIALIZED: AtomicBool = ATOMIC_BOOL_INIT;
+
 pub struct MonoLcd;
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum ColorButton {
     ButtonLeft,
     ButtonRight,
@@ -25,12 +31,43 @@ pub enum ColorButton {
     BttonMenu,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum MonoButton {
     Button0,
     Button1,
     Button2,
     Button3,
+}
+
+#[derive(Debug)]
+pub enum LcdError {
+    NotConnected,
+    Initialization,
+    MonoBackground,
+    MonoText,
+    ColorBackground,
+    ColorTitle,
+    ColorText,
+}
+
+impl Error for LcdError {
+    fn description(&self) -> &str {
+        match *self {
+            LcdError::NotConnected    => "LCD is not connected",
+            LcdError::Initialization  => "A FFI call to LogiLcdInit() has failed",
+            LcdError::MonoBackground  => "A FFI call to LogiLcdMonoSetBackground() has failed",
+            LcdError::MonoText        => "A FFI call to LogiLcdMonoSetText() has failed",
+            LcdError::ColorBackground => "A FFI call to LogiLcdColorSetBackground() has failed",
+            LcdError::ColorTitle      => "A FFI call to LogiLcdColorSetTitle() has failed",
+            LcdError::ColorText       => "A FFI call to LogiLcdColorSetText() has failed",
+        }
+    }
+}
+
+impl Display for LcdError {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        write!(f, "LcdError: {}", self.description())
+    }
 }
 
 impl ColorButton {
@@ -60,18 +97,30 @@ impl MonoButton {
 }
 
 impl MonoLcd {
-    pub fn connect(app_name: &str) -> Result<MonoLcd, ()> {
+    pub fn connect(app_name: &str) -> Result<MonoLcd, LcdError> {
+        assert_eq!(INITIALIZED.swap(true, Ordering::SeqCst), false);
+
         let ws = WideCString::from_str(app_name).unwrap();
-        unsafe {
+        let ret = unsafe {
             match LogiLcdInit(ws.as_ptr(), LcdType::MONO) {
                 Bool::TRUE => {
                     match LogiLcdIsConnected(LcdType::MONO) {
                         Bool::TRUE => Ok(MonoLcd),
-                        Bool::FALSE => Err(()),
+                        Bool::FALSE => Err(LcdError::NotConnected),
                     }
                 },
-                Bool::FALSE => Err(()),
+                Bool::FALSE => Err(LcdError::Initialization),
             }
+        };
+        if ret.is_err() {
+            INITIALIZED.store(false, Ordering::SeqCst);
+        }
+        ret
+    }
+
+    pub fn is_connected() -> bool {
+        unsafe {
+            LogiLcdIsConnected(LcdType::MONO).into()
         }
     }
 
@@ -87,23 +136,23 @@ impl MonoLcd {
         }
     }
 
-    pub fn set_background(&mut self, bytemap: &[u8]) -> Result<(), ()> {
+    pub fn set_background(&mut self, bytemap: &[u8]) -> Result<(), LcdError> {
         assert_eq!(bytemap.len(), MONO_WIDTH * MONO_HEIGHT);
         unsafe {
             match LogiLcdMonoSetBackground(bytemap.as_ptr()) {
                 Bool::TRUE  => Ok(()),
-                Bool::FALSE => Err(()),
+                Bool::FALSE => Err(LcdError::MonoBackground),
             }
         }
     }
 
-    pub fn set_text(&mut self, line_number: usize, text: &str) -> Result<(), ()> {
+    pub fn set_text(&mut self, line_number: usize, text: &str) -> Result<(), LcdError> {
         let ws = WideCString::from_str(text).unwrap();
         assert!(line_number < 4);
         unsafe {
             match LogiLcdMonoSetText(line_number as c_int, ws.as_ptr()) {
                 Bool::TRUE  => Ok(()),
-                Bool::FALSE => Err(()),
+                Bool::FALSE => Err(LcdError::MonoText),
             }
         }
     }
