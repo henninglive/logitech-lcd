2ec96d6582c3c661d4bb0e7ee8cf818660db8447
Henning Ottesen <henning@live.no>
2017-02-16 10:19:00 +0100

    Copy lib to output directory
    Resolves: #3

--- logi-lcd-sys/build.rs
+++ logi-lcd-sys/build.rs
@@ -3,7 +3,7 @@ extern crate winreg;
 use winreg::RegKey;
 use winreg::enums::*;
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::env;
 
 /*
@@ -44,8 +44,22 @@ fn main() {
     };
 
     let dll_path = Path::new(&dll_path_str[..]);
-
     assert!(dll_path.is_file());
+    assert_eq!(dll_path.file_name().unwrap(), "LogitechLcd.dll");
+
+    if env::var("CARGO_FEATURE_COPY_TO_OUTPUT").is_ok() {
+        let out_dir_str = env::var("OUT_DIR").unwrap();
+        let mut out_dll_path = PathBuf::from(out_dir_str);
+
+        assert!(out_dll_path.pop());
+        assert!(out_dll_path.pop());
+        assert!(out_dll_path.pop());
+
+        out_dll_path.push(dll_path.file_name().unwrap());
+        if !dll_path.exists() {
+            std::fs::copy(dll_path, out_dll_path).unwrap();
+        }
+    }
 
     let dir_str  = dll_path.parent().unwrap().to_str().unwrap();
 
