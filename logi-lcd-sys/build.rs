7f84b0b81e8696c049936d6802d3ebf99804f778
Henning Ottesen <henning@live.no>
2017-02-22 02:41:50 +0100

    Take dll dir insted of dll path as build var

--- logi-lcd-sys/build.rs
+++ logi-lcd-sys/build.rs
@@ -17,7 +17,7 @@ x86 HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Classes\CLSID\{d0e790a5-01a7-49ae-ae
 
 // Try and find path to LogitechLcd.dll searching for its CLSID
 // in the windows registry.
-fn dll_path_clsid() -> String{
+fn dll_path_clsid() -> String {
     assert!(cfg!(target_pointer_width = "64"), "Unsupported target platform");
 
     let hkcl = RegKey::predef(HKEY_CLASSES_ROOT);
@@ -29,40 +29,43 @@ fn dll_path_clsid() -> String{
                  manually specify a library path with the 'LOGITECH_LCD' environment
                  variable or by overriding build script in a manifest file");
 
-    key.get_value::<String, &str>("").expect(
+    let lib_path = key.get_value::<String, &str>("").expect(
         "The 'KEY_CLASSES_ROOT\\CLSID\\{d0e790a5-01a7-49ae-ae0b-e986bdd0c21b}\\ServerBinary' \
-        registry key is missing the '(default)' subkey")
+        registry key is missing the '(default)' subkey");
+
+    let lib_dir_path = Path::new(&lib_path[..]).parent().unwrap();
+    String::from(lib_dir_path.to_str().unwrap())
 }
 
 
 fn main() {
     assert!(cfg!(windows), "Unsupported target platform");
 
-    let dll_path_str = match env::var("LOGITECH_LCD") {
+    let lib_dir_str = match env::var("LOGITECH_LCD_LIB_DIR") {
         Ok(val) => val,
         Err(_)  => dll_path_clsid(),
     };
 
-    let dll_path = Path::new(&dll_path_str[..]);
-    assert!(dll_path.is_file());
-    assert_eq!(dll_path.file_name().unwrap(), "LogitechLcd.dll");
+    println!("lib_dir_str:{:?}", lib_dir_str);
+
+    let lib_path = Path::new(&lib_dir_str[..]).join("LogitechLcd.dll");
+    assert!(lib_path.is_file());
+    assert_eq!(lib_path.file_name().unwrap(), "LogitechLcd.dll");
 
     if env::var("CARGO_FEATURE_COPY_TO_OUTPUT").is_ok() {
         let out_dir_str = env::var("OUT_DIR").unwrap();
-        let mut out_dll_path = PathBuf::from(out_dir_str);
+        let mut out_lib_path = PathBuf::from(out_dir_str);
 
-        assert!(out_dll_path.pop());
-        assert!(out_dll_path.pop());
-        assert!(out_dll_path.pop());
+        assert!(out_lib_path.pop());
+        assert!(out_lib_path.pop());
+        assert!(out_lib_path.pop());
 
-        out_dll_path.push(dll_path.file_name().unwrap());
-        if !dll_path.exists() {
-            std::fs::copy(dll_path, out_dll_path).unwrap();
+        out_lib_path.push(lib_path.file_name().unwrap());
+        if !lib_path.exists() {
+            std::fs::copy(lib_path, out_lib_path).unwrap();
         }
     }
 
-    let dir_str  = dll_path.parent().unwrap().to_str().unwrap();
-
-    println!("cargo:rustc-link-search=native={}", dir_str);
+    println!("cargo:rustc-link-search=native={}", lib_dir_str);
     println!("cargo:rustc-link-lib=LogitechLcd");
 }
